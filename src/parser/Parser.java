59712aa3e61061bbf811ce6c918a42c43e965e27
Symmettry <109181931+Symmettry@users.noreply.github.com>
2024-04-19 00:54:26 +0200

    Nevermind that breaks things???

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -13,14 +13,14 @@ public class Parser {
     private int currentIndex = 0;
 
     private Lexer.Token eat() {
-        if(this.tokens.size() < currentIndex + 1) return new Lexer.Token(Lexer.TokenType.SEMICOLON, ";");
+        if(this.currentIndex > this.tokens.size()) return null;
         final Lexer.Token temp = this.tokens.get(currentIndex);
         this.tokens.set(currentIndex, null);
         currentIndex++;
         return temp;
     }
     private Lexer.Token at() {
-        return this.tokens.size() < currentIndex + 1 ? new Lexer.Token(Lexer.TokenType.SEMICOLON, ";") : this.tokens.get(currentIndex);
+        return this.currentIndex > this.tokens.size() ? new Lexer.Token(Lexer.TokenType.SEMICOLON, ";") : this.tokens.get(currentIndex);
     }
     @SuppressWarnings("preview")
     private Lexer.Token expect(final Lexer.TokenType type, final String error) {
@@ -96,6 +96,7 @@ public class Parser {
             return new AST.MemberExpr((AST.Identifier) val1, value);
         }
         if(this.at().type == Lexer.TokenType.SEMICOLON || this.at().type == Lexer.TokenType.CLOSE_ARR) { // a; a]
+            this.eat(); // eat semicolon
             return val1;
         }
 
